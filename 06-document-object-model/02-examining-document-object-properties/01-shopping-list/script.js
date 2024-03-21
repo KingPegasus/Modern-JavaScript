13650c709dd8f872deb6d6729d4121a87c4cb7a0
Raza Saleemi <rks143@gmail.com>
2024-03-21 23:25:49 +0500

    feat(dom): Examining document object properties.

---
+++ 06-document-object-model/02-examining-document-object-properties/01-shopping-list/script.js
@@ -0,0 +1,36 @@
+let output;
+
+output = document.documentElement;
+console.log(output);
+
+output = document.head;
+console.log(output);
+
+output = document.body;
+console.log(output);
+
+output = document.head.children;
+console.log(output);
+
+output = document.doctype;
+console.log(output);
+
+output = document.URL;
+console.log(output);
+
+output = document.characterSet;
+console.log(output);
+
+output = document.contentType;
+console.log(output);
+
+output = document.forms;
+console.log(output);
+
+output = document.forms[0].method;
+console.log(output);
+
+//output = document.links[0].href;
+
+output = document.images[0];
+console.log(output);
