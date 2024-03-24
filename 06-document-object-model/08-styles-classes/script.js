d0a696042748b6eded302ad39f8fdb1715f8ce3b
Raza Saleemi <rks143@gmail.com>
2024-03-24 11:18:37 +0500

    feat(styles-classes): Adding & Removing the Styles and Classes.

---
+++ 06-document-object-model/08-styles-classes/script.js
@@ -0,0 +1,21 @@
+const text = document.querySelector('p');
+const itemList = document.querySelector('.item-list');
+const items = itemList.querySelectorAll('li');
+
+function run() {
+  // className
+  console.log(itemList.className);
+  //text.className = 'card dark';
+
+  // classList
+  console.log(itemList.classList);
+  itemList.classList.forEach((c) => console.log(c));
+  // text.classList.add('dark');
+
+  text.classList.toggle('dark');
+  // text.classList.replace('card', 'dark');
+
+  // Change style
+  itemList.style.lineHeight = '3';
+}
+document.querySelector('button').onclick = run;
