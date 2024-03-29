dc78c93554b2ea76ca1484fb83bb1f0bb5db5d6b
Raza Saleemi <rks143@gmail.com>
2024-03-29 17:02:30 +0500

    feat(promise): Refactoring from callback to promise.

---
+++ 09-asynchronous-javascript/06-callback-to-promise/script.js
@@ -0,0 +1,32 @@
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post two' },
+];
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let error = false;
+      if (error) {
+        reject('Error Occured');
+      } else {
+        posts.push(post);
+        resolve();
+      }
+    }, 2000);
+  });
+}
+
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach(function (post) {
+      const div = document.createElement('div');
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector('#posts').appendChild(div);
+    });
+  }, 1000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post' })
+  .then(getPosts)
+  .catch((err) => console.error(err));
